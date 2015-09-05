ea648e8fbf6d92909e7f99b08e0d51ef60565ed8
YoungKwonJo <Young-Kwon.Jo@cern.ch>
2015-09-05 13:39:11 +0900

    update it

--- macros/FlatTree.h
+++ macros/FlatTree.h
@@ -22,7 +22,9 @@ struct FlatTree
 public:
   TTree* tree_;
   double value_;
+  double value2_;
   int runnumber_;
+  int dpid_,yyyyMMdd_,hhmmss_;
   long date_;
   stringsP name_;
  
@@ -38,8 +40,12 @@ void FlatTree::book(TTree* tree)
 {
   tree_ = tree;
   tree_->Branch("runnumber", &runnumber_, "runnumber/I");
+  tree_->Branch("dpid", &dpid_, "dpid/I");
+  tree_->Branch("hhmmss", &hhmmss_, "hhmmss/I");
+  tree_->Branch("yyyyMMdd", &yyyyMMdd_, "yyyyMMdd/I");
   tree_->Branch("date", &date_, "date/L");
   tree_->Branch("value", &value_, "value/D");
+  tree_->Branch("value2", &value2_, "value2/D");
   tree_->Branch("name",  name_  );
 }
 void FlatTree::clear()
@@ -48,6 +54,7 @@ void FlatTree::clear()
    date_=-999;
    date_=0;
    value_=-9999.;
+   value2_=-9999.;
 }
 FlatTree::~FlatTree()
 {
