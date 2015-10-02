5ff88d60d2f6a721d5e9f161343669da12a0821d
YoungKwonJo <Young-Kwon.Jo@cern.ch>
2015-10-02 20:58:51 +0900

    update it for all disk

--- macros/makeTreeHVHI.py
+++ macros/makeTreeHVHI.py
@@ -8,37 +8,66 @@ import datetime
 #HV_EN4_2015_2.txt
 
 gROOT.ProcessLine(".L FlatTree.h+");
+class merge():
+    def __init__(self):
+        self.data=[]
+        self.names=[]
+
+    def mergeJSON(self,Jsons):
+      for i,dataA in enumerate(Jsons):
+        if self.data == [] :
+          self.data = dataA
+          for j,adataA in enumerate(dataA):
+             self.names.append(adataA["name"])
+             print str(adataA["name"])
+        else :
+          for j,adataA in enumerate(dataA):
+            if self.data[i]["name"].find(adataA["name"])>-1:
+              self.data[i]["data"]+=adataA["data"]
+          #print adataA
+        #check missing name
+        for j,adataA in enumerate(dataA):
+          isFind=False
+          for k in self.names:
+            if k.find(adataA["name"])>-1 : isFind=True
+          if isFind==False:
+            self.names.append(adataA["name"])
+            self.data.append(adataA)
+            print str(adataA["name"])
+    def getData(self):
+        return self.data
 
 def getJSON(name_):
   jsons = []
   for i in range(1,9): 
     Name_ = (name_+"_2015_%d.txt"%i)
     json1_ = open(Name_)
-  
+    print Name_
     data2_ = json.load(json1_)
   
     data_ = data2_['data']
-  
+    #print data_
     jsons.append( data_)
   return jsons
 
-def mergeJSON(Jsons):
-  jsonM = []
-  for i,dataA in enumerate(Jsons):
-    if i==0: 
-      jsonM = dataA
+#def mergeJSON(Jsons):
+  #jsonM = []
+  #for i,dataA in enumerate(Jsons):
+    #if i==0:
+      #jsonM = dataA
       #print str(dataA[0])
-    else :
-      for j,adataA in enumerate(dataA):
-        if jsonM[i]["name"].find(adataA["name"])>-1:
-          jsonM[i]["data"]+=adataA["data"]
+    #else :
+      #for j,adataA in enumerate(dataA):
+       # if jsonM[i]["name"].find(adataA["name"])>-1:
+          #jsonM[i]["data"]+=adataA["data"]
+       # print adataA
 
-  return jsonM
+  #return jsonM
 
 ############################################
 def makeTree(jsonAm, jsonBm):
   tree_ = TTree("tree",'RPC WBM tree')
-  fevent_ = FlatTree()  
+  fevent_ =FlatTree()
   fevent_.book(tree_)
   ###############
   for i,dataA3 in enumerate(jsonAm):
@@ -81,18 +110,54 @@ def makeTree(jsonAm, jsonBm):
 
 
 
-jsonAs = getJSON("HV_EN4")
-jsonBs = getJSON("HI_EN4")
+jsonAs = getJSON("HV_EN1")
+jsonBs = getJSON("HI_EN1")
+jsonCs = getJSON("HV_EN2")
+jsonDs = getJSON("HI_EN2")
+jsonEs = getJSON("HV_EN3")
+jsonFs = getJSON("HI_EN3")
+jsonGs = getJSON("HV_EN4")
+jsonHs = getJSON("HI_EN4")
+jsonIs = getJSON("HV_EP1")
+jsonJs = getJSON("HI_EP1")
+jsonKs = getJSON("HV_EP2")
+jsonLs = getJSON("HI_EP2")
+jsonTs = getJSON("HV_EP3")
+jsonNs = getJSON("HI_EP3")
+jsonOs = getJSON("HV_EP4")
+jsonPs = getJSON("HI_EP4")
+
 #print "bbbbbbbbbbb"+str(jsonAs)+"bbbbbbbb"
 #print "aaaaaaaaaaaaaa"+str(jsonAs)+"aaaaaaaaa"
+jsonAm= merge();
+jsonBm=merge();
 
-jsonAm = mergeJSON(jsonAs)
-jsonBm = mergeJSON(jsonBs)
+jsonAm.mergeJSON(jsonAs)
+jsonBm.mergeJSON(jsonBs)
+jsonAm.mergeJSON(jsonCs)
+jsonBm.mergeJSON(jsonDs)
+jsonAm.mergeJSON(jsonEs)
+jsonBm.mergeJSON(jsonFs)
+jsonAm.mergeJSON(jsonGs)
+jsonBm.mergeJSON(jsonHs)
+jsonAm.mergeJSON(jsonIs)
+jsonBm.mergeJSON(jsonJs)
+jsonAm.mergeJSON(jsonKs)
+jsonBm.mergeJSON(jsonLs)
+jsonAm.mergeJSON(jsonTs)
+jsonBm.mergeJSON(jsonNs)
+jsonAm.mergeJSON(jsonOs)
+jsonBm.mergeJSON(jsonPs)
+FulljsonaftermargeAm=jsonAm.getData()
+FulljsonaftermargeBm=jsonAm.getData()
 #print "bbbbbbbbbbb"+str(jsonBm)+"bbbbbbbb"
+#print "bbbbbbbbbbb"+str(FulljsonaftermargeAm)+"bbbbbbbb"
+#print "bbbbbbbbbbb"+str(FulljsonaftermargeBm)+"bbbbbbbb"
 #print "aaaaaaaaaaaaaa"+str(jsonAm)+"aaaaaaaaa"
 
-f = TFile('tree_RPCWBM.root','RECREATE')
-tree_ = makeTree(jsonAm, jsonBm)
+f = TFile('RPCWBM.root','RECREATE')
+#tree_ = makeTree(jsonAm, jsonBm)
+tree_ = makeTree(FulljsonaftermargeAm, FulljsonaftermargeBm)
 f.Write()
 f.Close()
 
