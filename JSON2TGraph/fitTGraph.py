67ba03a38e7578043989d2541378ca7872a9530e
YoungKwonJo <cccpy98@gmail.com>
2016-06-08 14:05:36 +0200

    update it

--- JSON2TGraph/fitTGraph.py
+++ JSON2TGraph/fitTGraph.py
@@ -1,4 +1,5 @@
-from ROOT import TFile,TGraph,TCanvas,TLatex
+from ROOT import TFile,TGraph,TCanvas,TLatex,TLegend,TH1D
+from array import array
 import copy
 
 def addLegendText(text):
@@ -17,21 +18,51 @@ def getTGraphFlat(f1):
      tgs.append( copy.deepcopy(obj) )
   return tgs
 
-def fitTGraph(tg,idx):
+def fitTGraph(tg,idx,functexAll,tfs):
   print ""
   print ""
   c1 = TCanvas("c1"+str(idx),"",400,400)
   tg.Draw()
   tg.Fit("pol1")
   myfunc=tg.GetFunction("pol1")
+  print "myfunc "+str(type(myfunc))
+
+  myfunc.SetLineColor(idx)
+  myfunc.SetName(tg.GetName())
+
   p0=myfunc.GetParameter(0)
   p1=myfunc.GetParameter(1)
   functext="y = "+str(round(p0*1000)/1000)+"x + "+str(round(p1*100000)/100000.)
+  functexAll[tg.GetName()] = functext
+  tfs[tg.GetName()]={"p0":p0,"p1":p1}
 
   tex=addLegendText(functext)
   tex.Draw()
   c1.Print(tg.GetName()+".png")
   return c1,tg,tex
+
+def makeCanvasAll(name,tfs,pol):
+  c1 = TCanvas("c1"+name,"",1200,400)
+  x,y,z=[],[],[]
+  for i,xa in enumerate(tfs.keys()):
+    y.append(tfs[xa][pol])
+    x.append(i+1)
+    z.append(xa)
+
+  xx = array("d", x)
+  yy = array("d", y)
+  g = TGraph(len(xx), xx,yy)
+  xax = g.GetXaxis()
+  g.SetTitle(name+" of fit functions y=ax+b")
+
+  for i in range(len(xx)):
+    binIndex = xax.FindBin(i+1)
+    g.GetXaxis().SetBinLabel(binIndex,z[i])
+
+  g.Draw()
+
+  return c1,g
+
 
 def main():
   import sys
@@ -46,10 +77,17 @@ def main():
   tgs = getTGraphFlat(f)
 
   temp = {}
-  for i,tg in enumerate(tgs):
-    temp[i]=fitTGraph(tg,i)
+  functexAll = {}
+  tfs = {}
 
-  return temp
+  for i,tg in enumerate(tgs):
+    temp[i]=fitTGraph(tg,i,functexAll,tfs)
+
+  aaa = makeCanvasAll("a",tfs,"p0")
+  bbb = makeCanvasAll("b",tfs,"p1")
+
+  return functexAll,temp,tfs,aaa,bbb
+
 
 if __name__ == "__main__":
     test=main()
