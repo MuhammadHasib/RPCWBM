248a053072775836dcad34bfc72c192a016670e0
YoungKwonJo <cccpy98@gmail.com>
2016-06-08 12:20:56 +0200

    update it

--- JSON2TGraph/README.md
+++ JSON2TGraph/README.md
@@ -4,10 +4,9 @@ JSON to TGraph
 usage :
 > python RPCWBM_JSON2TGraph.py filename
 
-> python -i fitTGraph.py 
+> python -i fitTGraph.py filename
 
 for a example :
 > python RPCWBM_JSON2TGraph.py HV_current_BE
 
-> python -i fitTGraph.py 
-
+> python -i fitTGraph.py HV_current_BE
--- JSON2TGraph/fitTGraph.py
+++ JSON2TGraph/fitTGraph.py
@@ -1,5 +1,12 @@
-from ROOT import TFile,TGraph,TCanvas
+from ROOT import TFile,TGraph,TCanvas,TLatex
 import copy
+
+def addLegendText(text):
+  tex2 = TLatex(-20.,50.,text)
+  tex2.SetX(0.25),        tex2.SetY(0.83)
+  tex2.SetNDC(),          tex2.SetTextAlign(12)
+  tex2.SetTextColor(2),   tex2.SetTextFont(42) 
+  return tex2
 
 def getTGraphFlat(f1):
   tgs = []
@@ -16,16 +23,35 @@ def fitTGraph(tg,idx):
   c1 = TCanvas("c1"+str(idx),"",400,400)
   tg.Draw()
   tg.Fit("pol1")
+  myfunc=tg.GetFunction("pol1")
+  p0=myfunc.GetParameter(0)
+  p1=myfunc.GetParameter(1)
+  functext="y = "+str(round(p0*1000)/1000)+"x + "+str(round(p1*100000)/100000.)
+
+  tex=addLegendText(functext)
+  tex.Draw()
   c1.Print(tg.GetName()+".png")
-  return c1,tg
+  return c1,tg,tex
 
-import os
-loc = os.getcwd()
-f = TFile.Open(loc+"/HV_current_BE.root")
-tgs = getTGraphFlat(f)
+def main():
+  import sys
+  if len(sys.argv) < 2:
+    print "usage : python fitTGraph.py filename"
+    sys.exit()
 
-temp = {}
-for i,tg in enumerate(tgs):
-  temp[i]=fitTGraph(tg,i)
+  filename = sys.argv[1]
+  import os
+  loc = os.getcwd()
+  f = TFile.Open(loc+"/"+filename+".root")
+  tgs = getTGraphFlat(f)
+
+  temp = {}
+  for i,tg in enumerate(tgs):
+    temp[i]=fitTGraph(tg,i)
+
+  return temp
+
+if __name__ == "__main__":
+    test=main()
 
 
