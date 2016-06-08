febb783cab2ed9cc588964f41fda50fdefa68cda
YoungKwonJo <cccpy98@gmail.com>
2016-06-08 12:02:17 +0200

    udpate it

--- JSON2TGraph/fitTGraph.py
+++ JSON2TGraph/fitTGraph.py
@@ -1,24 +1,31 @@
 from ROOT import TFile,TGraph,TCanvas
+import copy
+
+def getTGraphFlat(f1):
+  tgs = []
+  for key in [x.GetName() for x in f1.GetListOfKeys()]:
+    obj = f1.Get(key)
+    if obj == None: continue
+    elif obj.IsA().InheritsFrom("TGraph"):
+     tgs.append( copy.deepcopy(obj) )
+  return tgs
+
+def fitTGraph(tg,idx):
+  print ""
+  print ""
+  c1 = TCanvas("c1"+str(idx),"",400,400)
+  tg.Draw()
+  tg.Fit("pol1")
+  c1.Print(tg.GetName()+".png")
+  return c1,tg
 
 import os
 loc = os.getcwd()
 f = TFile.Open(loc+"/HV_current_BE.root")
+tgs = getTGraphFlat(f)
 
-tg = f.Get("Barrel")
-tg2 = f.Get("Endcap")
+temp = {}
+for i,tg in enumerate(tgs):
+  temp[i]=fitTGraph(tg,i)
 
-c1 = TCanvas("c1","",800,400)
-c1.Divide(2,1)
-c1.cd(1)
-print tg.GetName()
-tg.Draw()
-tg.Fit("pol1")
-
-print ""
-print ""
-
-c1.cd(2)
-print tg2.GetName()
-tg2.Draw()
-tg2.Fit("pol1")
 
